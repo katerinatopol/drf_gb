3f7cbc7b592b288788e1fb3f85feeeff803584f3
katerina_topol <katerina_topol@mail.ru>
2021-09-23 19:35:29 +0300

    add lesson 2

---
+++ frontend/src/components/Users.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>{user.username}</td>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table className="table">
+            <tr>
+                <th>User Name</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            </tr>
+            {users.map((u) => <UserItem user={u} />)}
+        </table>
+    )
+}
+
+export default UserList
