9e14ebd95dd78132fbc6bb5c40ae7791884cde1b
katerina_topol <katerina_topol@mail.ru>
2021-09-29 14:53:02 +0300

    add ViewSets

--- backend/todoapp/views.py
+++ backend/todoapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Project, TODO
+from .serializers import ProjectSerializer, TODOSerializer
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
+
+
+class TODOViewSet(ModelViewSet):
+    serializer_class = TODOSerializer
+    queryset = TODO.objects.all()
+    pagination_class = TODOLimitOffsetPagination
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
+
+
+class ProjectViewSet(ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+    filterset_fields = ['name']
+    pagination_class = ProjectLimitOffsetPagination
