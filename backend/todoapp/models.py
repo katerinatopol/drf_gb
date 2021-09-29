4adb0f274a23552ba78344e20de74128b1423d09
katerina_topol <katerina_topol@mail.ru>
2021-09-29 14:19:30 +0300

    add model project

--- backend/todoapp/models.py
+++ backend/todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from backend.userapp import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+    )
+    repository = models.URLField(
+        blank=True,
+    )
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
