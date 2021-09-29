85dae71ae8f4340552dce5ed39a22d353417b436
katerina_topol <katerina_topol@mail.ru>
2021-09-29 14:31:22 +0300

    add model todo

--- backend/todoapp/models.py
+++ backend/todoapp/models.py
@@ -14,3 +14,30 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TODO(models.Model):
+    project = models.OneToOneField(
+        Project,
+        on_delete=models.CASCADE,
+    )
+    text = models.CharField(
+        max_length=64,
+    )
+    data_create = models.DateTimeField(
+        auto_now_add=True,
+    )
+    data_update = models.DateTimeField(
+        auto_now=True,
+    )
+
+    author = models.OneToOneField(
+        User,
+        on_delete=models.PROTECT,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    def __str__(self):
+        return self.text
