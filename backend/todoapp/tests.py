c687025d41f303d84079b1cb0c50b77bf4a60702
katerina_topol <katerina_topol@mail.ru>
2021-10-18 16:54:39 +0300

    add tests

--- backend/todoapp/tests.py
+++ backend/todoapp/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase, APISimpleTestCase, APIRequestFactory, force_authenticate
 
-# Create your tests here.
+from todoapp.models import TODO
+from userapp.models import User
+
+from django.contrib.auth.models import User
+from mixer.backend.django import mixer
+
+
+class MixerTestCase(APITestCase):
+
+    def test_todo_list(self):
+        for i in range(100):
+            mixer.blend(TODO, project__name='Test')
+
+
+# APIClient
+class TODOTestCase(APIClient):
+
+    def test_user_post(self):
+        User.objects.create_superuser('Supertest', 'super@test.com', 'qwerty')
+        client = APIClient()
+        client.login(username='Supertest', password='qwerty')
+
+        res = client.post('/api/todos/', {
+            'project': 1,
+            'text': 'test-text'})
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        todo = TODO.objects.get(id=res.data['id'])
+        self.assertEqual(todo.text, 'test-text')
--- backend/userapp/tests.py
+++ backend/userapp/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase, APISimpleTestCase, APIRequestFactory, force_authenticate
+from userapp.models import User
+from .views import UserViewSet
+from django.contrib.auth.models import User
+from mixer.backend.django import mixer
 
-# Create your tests here.
+
+# mixer
+class MixerTestCase(APITestCase):
+
+    def test_user_list(self):
+        for i in range(100):
+            mixer.blend(User, project=1)
+
+
+# APITestCase
+class UserTestCase(APITestCase):
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser('Supertest', 'super@test.com', 'qwerty')
+        self.client.login(username='Supertest', password='qwerty')
+
+    def test_user_list(self):
+        User.objects.create(first_name='Ivan', last_name='Ivanov', email='ivan@ivan.ru')
+        res = self.client.get('/api/users/')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.client.logout()
+        res = self.client.get('/api/authors/')
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_post(self):
+        res = self.client.post('/api/users/', {
+            'first_name': 'Ivan',
+            'last_name': 'Ivanov',
+            'email': 'ivan@ivan.ru'})
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = User.objects.get(id=res.data['id'])
+        self.assertEqual(user.last_name, 'Ivanov')
+
+    # APIRequestFactory
+    def test_factory(self):
+        factory = APIRequestFactory()
+        view = UserViewSet.as_view({'get': 'list'})
+        request = factory.get('/api/authors/')
+        res = view(request)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        request = factory.get('/api/authors/')
+        force_authenticate(request, self.admin)
+        res = view(request)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+# class FuncTest(APISimpleTestCase):
+#
+#     def test_func(self):
+#         self.assertTrue(True)
